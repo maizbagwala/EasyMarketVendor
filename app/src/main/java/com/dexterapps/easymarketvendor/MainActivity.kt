8ce82829d397d7a0c24d3e6260258fdc56f73cd0
maizbagwala <44838294+maizbagwala@users.noreply.github.com>
2021-03-18 14:40:40 +0530

    m

--- app/src/main/java/com/dexterapps/easymarketvendor/MainActivity.kt
+++ app/src/main/java/com/dexterapps/easymarketvendor/MainActivity.kt
@@ -2,15 +2,24 @@ package com.dexterapps.easymarketvendor
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        nav_btn = findViewById(R.id.nav_btn)
+        drawer = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        mFragmentManager = supportFragmentManager
     }
 
     fun onNavClick(view: View) {
@@ -19,6 +28,19 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "order history clicked", Toast.LENGTH_SHORT).show()
             }
 
+        }
+    }
+
+
+    companion object {
+        lateinit var nav_btn: ImageView
+        lateinit var drawer: DrawerLayout
+        lateinit var navigationView: NavigationView
+        lateinit var mFragmentManager: FragmentManager
+        fun loadFragment(fragment: Fragment, tag: String) {
+            mFragmentManager.beginTransaction().replace(R.id.frag_host, fragment)
+                .addToBackStack(tag)
+                .commit()
         }
     }
 }
