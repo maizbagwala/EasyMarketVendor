04685ddc27388d57d4fee2107bc921b27b5b6d0a
maiz bagwala <44838294+maizbagwala@users.noreply.github.com>
2021-03-30 19:16:26 +0530

    add product added

--- app/src/main/java/com/dexterapps/easymarketvendor/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/dexterapps/easymarketvendor/addproduct/AddProductFragment.kt
@@ -1,19 +1,31 @@
 package com.dexterapps.easymarketvendor.addproduct
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dexterapps.easymarketvendor.R
 import com.dexterapps.easymarketvendor.addproduct.adapter.AddProductAdapter
+import com.jama.carouselview.CarouselView
+import com.jama.carouselview.enums.IndicatorAnimationType
+import com.jama.carouselview.enums.OffsetType
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +33,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class AddProductFragment : Fragment() {
+    var viewData = arrayOfNulls<View>(5)
+    lateinit var uri: Uri
+    var clicked_position: Int = -1
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -42,8 +58,90 @@ class AddProductFragment : Fragment() {
         val rvSubCategory: RecyclerView = view.findViewById(R.id.rv_sub_category)
         rvSubCategory.layoutManager = LinearLayoutManager(context)
         rvSubCategory.adapter = AddProductAdapter()
+        val carouselView = view.findViewById<CarouselView>(R.id.carousel)
+        carouselView.apply {
+            size = 5
+            resource = R.layout.image_carousel_item
+            indicatorAnimationType = IndicatorAnimationType.THIN_WORM
+            carouselOffset = OffsetType.CENTER
 
+
+            setCarouselViewListener { view, position ->
+
+                view.findViewById<CardView>(R.id.image_upload_card).setOnClickListener {
+                    val intent = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                    )
+
+                    viewData[position] = view
+
+
+                    intent.type = "image/*"
+                    intent.putExtra("crop", "true")
+                    intent.putExtra("scale", true)
+                    intent.putExtra("outputX", 256)
+                    intent.putExtra("outputY", 256)
+                    intent.putExtra("aspectX", 1)
+                    intent.putExtra("aspectY", 1)
+                    intent.putExtra("return-data", true)
+                    startActivityForResult(intent, position)
+
+                }
+            }
+            // After you finish setting up, show the CarouselView
+            show()
+        }
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null) {
+            when (requestCode) {
+                0 -> {
+                    clicked_position = 0
+                    uri = data.data!!
+                    Toast.makeText(context, "0", Toast.LENGTH_SHORT).show()
+                    viewData[0]?.findViewById<ImageView>(R.id.iv_upload_image)?.setImageURI(uri)
+//                    viewData[0].findViewById<View>(R.id.iv_upload_image)!!
+//                        .resources(context!!.getDrawable(R.drawable.app_logo))
+                }
+                1 -> {
+                    clicked_position = 1
+
+                    uri = data.data!!
+                    Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                    viewData[1]?.findViewById<ImageView>(R.id.iv_upload_image)?.setImageURI(uri)
+
+                }
+                2 -> {
+                    clicked_position = 2
+
+                    uri = data.data!!
+                    Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                    viewData[2]?.findViewById<ImageView>(R.id.iv_upload_image)?.setImageURI(uri)
+
+                }
+                3 -> {
+                    clicked_position = 3
+
+                    uri = data.data!!
+                    Toast.makeText(context, "3", Toast.LENGTH_SHORT).show()
+                    viewData[3]?.findViewById<ImageView>(R.id.iv_upload_image)?.setImageURI(uri)
+
+                }
+                4 -> {
+
+                    clicked_position = 4
+                    uri = data.data!!
+                    Toast.makeText(context, "4", Toast.LENGTH_SHORT).show()
+                    viewData[4]?.findViewById<ImageView>(R.id.iv_upload_image)?.setImageURI(uri)
+
+                }
+            }
+
+        }
     }
 
     companion object {
--- app/src/main/java/com/dexterapps/easymarketvendor/addproduct/adapter/AddProductAdapter.kt
+++ app/src/main/java/com/dexterapps/easymarketvendor/addproduct/adapter/AddProductAdapter.kt
@@ -3,8 +3,14 @@ package com.dexterapps.easymarketvendor.addproduct.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
+import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dexterapps.easymarketvendor.R
+
 
 class AddProductAdapter() :
     RecyclerView.Adapter<AddProductAdapter.AddProductViewHolder>() {
@@ -17,13 +23,39 @@ class AddProductAdapter() :
 
     override fun onBindViewHolder(holder: AddProductViewHolder, position: Int) {
 
+        val types = arrayOf("GM", "KG")
+        val adapterType = ArrayAdapter(
+            holder.itemView.context,
+            R.layout.my_simple_spinner_item,
+            types
+        )
+        holder.typeSpinner.adapter = adapterType
+
+        val discountType = arrayOf("%", "$")
+        val adapterDisc = ArrayAdapter(
+            holder.itemView.context,
+            R.layout.my_simple_spinner_item,
+            discountType
+        )
+        holder.discountSpinner.adapter = adapterDisc
+
+
     }
+
 
     override fun getItemCount(): Int {
         return 3
     }
 
     class AddProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val etType: EditText = itemView.findViewById(R.id.et_type)
+        val etMrp: EditText = itemView.findViewById(R.id.et_mrp)
+        val etDiscount: EditText = itemView.findViewById(R.id.et_discount)
+        val etDescription: EditText = itemView.findViewById(R.id.et_description)
+        val btnDelete: ImageView = itemView.findViewById(R.id.iv_delete)
+        val inStock: SwitchCompat = itemView.findViewById(R.id.switch_inStock)
+        val typeSpinner: Spinner = itemView.findViewById(R.id.spinner_type)
+        val discountSpinner: Spinner = itemView.findViewById(R.id.spinner_discount)
 
     }
 }
