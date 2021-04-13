83bec1f44c8cdcacd307b947568b6a5781d71e2a
maizbagwala <44838294+maizbagwala@users.noreply.github.com>
2021-04-13 21:03:34 +0530

    Update NewOrderTabFragment.kt

--- app/src/main/java/com/dexterapps/easymarketvendor/home/NewOrderTabFragment.kt
+++ app/src/main/java/com/dexterapps/easymarketvendor/home/NewOrderTabFragment.kt
@@ -15,28 +15,9 @@ import com.dexterapps.easymarketvendor.config.Variables
 import com.dexterapps.easymarketvendor.home.adapter.NewOrderAdapter
 import com.dexterapps.easymarketvendor.home.model.NewOrderModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewOrderTabFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewOrderTabFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,13 +26,13 @@ class NewOrderTabFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_new_order_tab, container, false)
 
-        MainActivity.hideShow(Variables.NAME_NEW_ORDER,false)
+        MainActivity.hideShow(Variables.NAME_NEW_ORDER, false)
         MainActivity.nav_back_btn.setOnClickListener {
             MainActivity.back()
         }
-        var tabAll: TextView
-        var tabPending: TextView
-        var tabAccepted: TextView
+        val tabAll: TextView
+        val tabPending: TextView
+        val tabAccepted: TextView
 
         val rvNewOrder: RecyclerView = view.findViewById(R.id.rv_new_order)
         val b_tab_dashboard: LinearLayout = view.findViewById(R.id.b_tab_dashboard)
@@ -63,8 +44,6 @@ class NewOrderTabFragment : Fragment() {
         tabPending = view.findViewById(R.id.tab_pending)
         tabAccepted = view.findViewById(R.id.tab_accepted)
         b_tab_dashboard.setOnClickListener {
-
-
             MainActivity.loadFragment(DashboardTabFragment(), Variables.TAG_DASHBOARD)
         }
 
@@ -95,36 +74,10 @@ class NewOrderTabFragment : Fragment() {
             tabPending.setBackgroundResource(R.drawable.greenbg_cornner)
             tabAccepted.setBackgroundResource(R.drawable.corner_view)
         }
-
-
-
-
-
-
-
-
-
+        
 
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewOrderTabFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewOrderTabFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
